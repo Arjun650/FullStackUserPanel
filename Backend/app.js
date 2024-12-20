c609ceb09a168888de9e3372cc059611e4e2e4bb
Arjun650 <darjun650@gmail.com>
2024-12-20 23:03:01 +0530

    final

--- Backend/app.js
+++ Backend/app.js
@@ -6,13 +6,7 @@ import orderProduct from './routes/placeOrder.routes.js'
 
 const app = express()
 
-app.use(cors(
-    {
-    origin:["https://deploy-mern-1whq.vercel.app"],
-    methods:["POST", "GET"],
-    credentials:true  
-    }
-));
+app.use(cors());
 
 app.use(express.json())
 
